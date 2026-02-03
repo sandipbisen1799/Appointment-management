46fdba37af82c8e4527aa0977c884da6e3d5b2c6
sandipbisen1799 <sandipbisen1799@gmail.com>
2026-02-03 12:23:45 +0530

    fix: update CORS origin to use production URL

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,7 +9,7 @@ const app = express()
 
 app.use(
   cors({
-       origin: ["http://localhost:5173", "http://localhost:5174",'https://appointment-management.vercel.app'],
+ origin: "https://appointment-management-pkrw.onrender.com",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
