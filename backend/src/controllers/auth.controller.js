eec314aee3d57b2fbf1d6643d0ea43d18698edd6
sandipbisen1799 <sandipbisen1799@gmail.com>
2026-02-09 13:32:15 +0530

    fix: update cookie settings in login function to include sameSite attribute

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -97,6 +97,7 @@ export const login = async (req,res)=>{
         return res.cookie("token", token, {
             httpOnly: true,
             secure: true,
+            sameSite:'none',
             maxAge: 24 * 60 * 60 * 1000,
         }).status(200).json({
             success:true,
